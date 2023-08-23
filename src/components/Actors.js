3cf2b1c7f79785d6b86bf260c560d949279f7aba
WanjikuN <cikunjoroge4@gmail.com>
2023-08-23 22:19:45 +0300

    Refactor: JSX on components and routing

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,17 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1 > Actors Page </h1>
+    {actors.map((act,index)=>{
+      return <div key={index}>
+        <p>{act.name} </p>
+        <ul>{act.movies.map((movie,index)=>{
+          return <li key={index}>{movie}</li>
+        })}</ul>
+      </div>
+    })}
+  </div>;
 }
 
 export default Actors;
